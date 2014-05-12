a109d7c55078b3043a0678bbf255822a1f63f4d5
Tyler Kennedy <tk@tkte.ch>
2014-05-12 16:10:29 -0400

    Configure a local SQLite database by default.

--- ein/config.py
+++ ein/config.py
@@ -3,6 +3,17 @@
 """
 Default configuration for ein.io.
 """
+import os
+import os.path
+
 #: True if Ein should handle the routing of static assets - suitable only
 #: for debugging and extremely low traffic sites.
 ROUTE_STATIC_ASSETS = True
+
+#: The SQLAlchemy connection URI. Do _NOT_ use SQLite in production.
+SQLALCHEMY_DATABASE_URI = 'sqlite:///{cwd}/ein_testing.db'.format(
+    cwd=os.path.abspath(os.getcwd())
+)
+
+#: Log all SQL statements to stderr. Invaluable for debugging.
+SQLALCHEMY_ECHO = False
