d5da4e54d011fc0c46d52a06a3a4557a40e2c19e
Tyler Kennedy <tk@tkte.ch>
2014-05-12 16:15:01 -0400

    Explict tablename for User model.

--- ein/models/user.py
+++ ein/models/user.py
@@ -8,6 +8,8 @@ from ein.server import db
 
 
 class User(db.Model):
+    __tablename__ = 'users'
+
     #: The unique, auto-incrementing identifier for a user.
     id = db.Column(db.Integer, primary_key=True)
 
